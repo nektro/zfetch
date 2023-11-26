fc0874c50237656020937622ea7c15df9356de12
Meghan Denny <hello@nektro.net>
2023-11-25 23:49:39 -0800

    always use Zigmod

--- build.zig
+++ build.zig
@@ -1,10 +1,7 @@
 const std = @import("std");
+const packages = @import("./deps.zig");
 
-const Builder = std.build.Builder;
-
-const packages = @import("deps.zig");
-
-pub fn build(b: *Builder) void {
+pub fn build(b: *std.build.Builder) void {
     const mode = b.option(std.builtin.Mode, "mode", "") orelse .Debug;
     const target = b.standardTargetOptions(.{});
 
@@ -13,59 +10,12 @@ pub fn build(b: *Builder) void {
         .target = target,
         .optimize = mode,
     });
-
-    if (@hasDecl(packages, "use_submodules")) { // submodules
-        const package = getPackage(b) catch unreachable;
-
-        for (package.module.dependencies.keys(), package.module.dependencies.values()) |name, module| {
-            lib_tests.addModule(name, module);
-        }
-    } else if (@hasDecl(packages, "addAllTo")) { // zigmod
-        packages.addAllTo(lib_tests);
-    } else if (@hasDecl(packages, "pkgs") and @hasDecl(packages.pkgs, "addAllTo")) { // gyro
-        packages.pkgs.addAllTo(lib_tests);
+    const moddep_deps = packages.packages[0].zp(b).module.dependencies;
+    for (moddep_deps.keys(), moddep_deps.values()) |name, module| {
+        lib_tests.addModule(name, module);
     }
 
     const tests = b.step("test", "Run all library tests");
     const tests_run = b.addRunArtifact(lib_tests);
     tests.dependOn(&tests_run.step);
-}
-
-fn getBuildPrefix() []const u8 {
-    return std.fs.path.dirname(@src().file) orelse ".";
-}
-
-fn getDependency(b: *Builder, comptime name: []const u8, comptime root: []const u8) !std.build.ModuleDependency {
-    const path = comptime getBuildPrefix() ++ "/libs/" ++ name ++ "/" ++ root;
-
-    // Make sure that the dependency has been checked out.
-    std.fs.cwd().access(path, .{}) catch |err| switch (err) {
-        error.FileNotFound => {
-            std.log.err("zfetch: dependency '{s}' not checked out", .{name});
-
-            return err;
-        },
-        else => return err,
-    };
-
-    return .{
-        .name = name,
-        .module = b.addModule(name, .{ .source_file = .{ .path = path } }),
-    };
-}
-
-pub fn getPackage(b: *Builder) !std.build.ModuleDependency {
-    var dependencies = b.allocator.alloc(std.build.ModuleDependency, 3) catch @panic("oom");
-
-    dependencies[0] = try getDependency(b, "iguanaTLS", "src/main.zig");
-    dependencies[1] = try getDependency(b, "uri", "uri.zig");
-    dependencies[2] = try getDependency(b, "hzzp", "src/main.zig");
-
-    return .{
-        .name = "zfetch",
-        .module = b.addModule("zfetch", .{
-            .source_file = .{ .path = comptime getBuildPrefix() ++ "/src/main.zig" },
-            .dependencies = dependencies,
-        }),
-    };
 }
